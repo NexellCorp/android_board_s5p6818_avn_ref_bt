54120ba02450f393ff66984c8bc073afa943ebb7
allan.park <allan.park@nexell.co.kr>
2016-11-01 19:30:27 +0900

    [ADD] add flash system to fastboot.sh

--- fastboot.sh
+++ fastboot.sh
@@ -2,5 +2,6 @@ fastboot flash partmap device/nexell/s5p6818_avn_ref_bt/partmap.txt
 fastboot flash 2ndboot result/2ndboot.bin
 fastboot flash bootloader result/u-boot.bin
 fastboot flash boot result/boot.img
+fastboot flash system result/system.img
 fastboot flash root result/root.img
 fastboot reboot
