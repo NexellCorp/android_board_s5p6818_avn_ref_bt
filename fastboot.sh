a328e78ef9691eb7ad98c6a6166a170235eaa425
allan.park <allan.park@nexell.co.kr>
2016-11-02 20:45:18 +0900

    [FIX] add sudo command to fastboot.sh

--- fastboot.sh
+++ fastboot.sh
@@ -1,8 +1,8 @@
-fastboot flash partmap device/nexell/s5p6818_avn_ref_bt/partmap.txt
-fastboot flash 2ndboot result/2ndboot.bin
-fastboot flash bootloader result/u-boot.bin
-fastboot flash boot result/boot.img
-fastboot flash system result/system.img
-fastboot flash cache result/cache.img
-fastboot flash userdata result/userdata.img
-fastboot reboot
+sudo fastboot flash partmap device/nexell/s5p6818_avn_ref_bt/partmap.txt
+sudo fastboot flash 2ndboot result/2ndboot.bin
+sudo fastboot flash bootloader result/u-boot.bin
+sudo fastboot flash boot result/boot.img
+sudo fastboot flash system result/system.img
+sudo fastboot flash cache result/cache.img
+sudo fastboot flash userdata result/userdata.img
+sudo fastboot reboot
